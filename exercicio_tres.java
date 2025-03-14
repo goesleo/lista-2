9b6449149d1245f61fa6fdb082bdb39c8b89207e
vai leo <leonardotaqua48@gmail.com>
2025-03-13 21:51:44 -0300

    exercicio pronto

--- exercicio_tres.java
+++ exercicio_tres.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class exercicio_tres {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero inteiro: ");
+        int numero = sc.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("o numero " + numero + " é par.");
+        }else{
+            System.out.println("o numero " + numero + " é impar.");
+        }
 
 
     }
